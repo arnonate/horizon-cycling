f5a6888d41fc17834446a6f006f8c9f86cf30184
Nate Arnold <nate.arnold@equipmentshare.com>
2020-02-20 12:40:09 -0600

    Remove sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
-    description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution."
+    title: "Horizon Cycling",
+    description: "The best damn bike shop in the midwest"
   },
   plugins: [
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sass",
+    "gatsby-plugin-styled-components",
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: "gatsby-source-filesystem",
@@ -44,9 +43,6 @@ module.exports = {
           {
             resolve: "gatsby-remark-images",
             options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
               maxWidth: 2048
             }
           },
@@ -65,13 +61,6 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`
       }
     },
-    {
-      resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
-      options: {
-        develop: true, // Activates purging in npm run develop
-        purgeOnly: ["/all.sass"] // applies purging only on the bulma css file
-      }
-    }, // must be after other CSS plugins
     "gatsby-plugin-netlify" // make sure to keep it last in the array
   ]
 };
